1212071b1377349c36b486faaaa87813517aa9d1
dgasch512 <gaschdav208@gmail.com>
2020-01-05 22:26:45 -0600

    started npm-node project

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,16 +1,27 @@
 const isEven = (num) => {
   // write code for numbers.isEven
-
+  // use modulus to determine even or odd
+  if (num % 2 === 0) {
+    return true;
+  }
+  return false
 }
 
 const sum = (arr) => {
   // write code for numbers.sum
-
+  return arr.reduce((a,b) => a +b, 0)
 }
 
 const comboSum = (arr, sum) => {
   // write code for numbers.comboSum
-
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === sum) {
+        return true
+      }
+    }
+  }
+  return false
 }
 
 module.exports = {
